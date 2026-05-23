8f0f43e27c0d428aa98697548fac51b5624a671a
AbsarAhmedBhutta <absarbhutta@gmail.com>
2026-05-23 21:56:05 +0500

    refactoring

---
+++ python specific/longest-consecutive-sequence.py
@@ -0,0 +1,12 @@
+# 5. Longest Consecutive Sequence
+# Given an unsorted array, find the length of the longest consecutive sequence.
+# Must run in O(n).
+# Input:
+# [100,4,200,1,3,2]
+# Output:
+# 4
+# (1,2,3,4)
+
+def longest_consecutive_sequence(input):
+    
+input = [100,4,200,1,3,2]
